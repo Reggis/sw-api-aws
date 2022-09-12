c24e76fe1cb4ec2e8a4f8658bdfe4edf31cb5bf7
Reggis <reggis2@gmail.com>
2022-09-12 12:21:59 +0200

    CleanUp

--- lib/get-collection.ts
+++ lib/get-collection.ts
@@ -1,14 +1,13 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
-import { validateIdInPath } from './common/parsers';
 import { CharacterRepository } from './repository/characterRepository';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    const lastEvaluatedKey = event.queryStringParameters ? event.queryStringParameters['lastEvaluatedKey']: undefined;
+    const lastEvaluatedKey = event.queryStringParameters ? event.queryStringParameters['lastEvaluatedKey'] : undefined;
     const characterRepository = new CharacterRepository();
 
     const collection = await characterRepository.getCharactersColletion(lastEvaluatedKey);
     return {
         statusCode: 200,
-        body: JSON.stringify({'characters': collection.Items, 'lastEvaluatedKey': collection.LastEvaluatedKey})
+        body: JSON.stringify({ 'characters': collection.Items, 'lastEvaluatedKey': collection.LastEvaluatedKey })
     };
 };
